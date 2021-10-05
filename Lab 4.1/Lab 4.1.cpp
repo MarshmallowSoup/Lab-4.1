9058f76444fc758dc67c451bb79d08393174d8d4
MarshmallowSoup <lysetskyyv@gmail.com>
2021-10-05 23:38:14 +0300

    first lab 4.1 commit

---
+++ Lab 4.1/Lab 4.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+ 
+int main(){
+	double i, S, N;
+	cout << "N = "; cin >> N;
+
+	i = N;
+	S = 0;
+		
+	while (i <= 22) {
+		S += sqrt(i * i + N * N) / i;
+		i++;
+	}
+
+	cout << "S = " << S << endl;
+	i = N;
+	S = 0;
+
+	do {
+		S += sqrt(i * i + N * N) / i;
+		i++;
+	} while (i <= 22);
+
+	cout << "S = " << S << endl;
+	i = N;
+	S = 0;
+
+	for (i = N; i <= 22; i++)
+	{
+		S += sqrt(i * i + N * N) / i;
+	}
+	cout << "S = " << S << endl;
+	S = 0;
+
+	for (i = 22; i >= N; i--)
+	{
+		S += sqrt(i * i + N * N) / i;
+	}
+
+	cout << "S = " << S << endl;
+	S = 0;
+
+	return 0;
+}
